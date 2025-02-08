28578c179828d52d073080cbc04f7c29474306a1
Novakboh-step <novak_dv82@student.itstep.org>
2025-02-09 01:07:35 +0200

    Added file system

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
 from Note import Note
+from datetime import datetime
+
+def saveNotes(notes):
+    with open("notes.txt", "a") as fileHandler:
+        for i in range(len(notes)):
+            fileHandler.writelines(f"{notes[i].getNote()}\n")
+    return []
+
+def getNotes():
+    with open("notes.txt") as fileHandler:
+        return fileHandler.read().split("\n")
+
+def printNotes():
+    with open("notes.txt") as fileHandler:
+        print(f"{fileHandler.read()}")
 
 notes = []
 while True:
@@ -8,19 +23,55 @@ while True:
     print("3 - delete note")
     print("4 - change head")
     print("5 - change text")
-    print("6 - exit")
+    print("6 - save notes")
+    print("7 - exit")
     a = input("Your choice: ")
     if a == "1":
-        for i in range(len(notes)):
-            notes[i].printNote()
+        notes = saveNotes(notes)
+        printNotes()
     elif a == "2":
         notes.append(Note(input("Enter head: "), input("Enter text: ")))
     elif a == "3":
+        saveNotes(notes)
+        notes = getNotes()
         notes.pop(int(input("Enter index: ")) - 1)
+        with open("notes.txt", "w") as fileHandler:
+            i = 0
+            while notes[i] != "":
+                fileHandler.writelines(f"{notes[i]}\n")
+                i += 1
+        notes = []
     elif a == "4":
-        notes[int(input("Enter index: ")) - 1].changeHead(input("Enter head: "))
+        saveNotes(notes)
+        notes = getNotes()
+        i = int(input("Enter index: ")) - 1
+        note = notes[i].split(" ")
+        note[0] = input("Enter head: ")
+        t = str(datetime.now()).split(".")[0]
+        notes[i] = f"{note[0]}: {note[1]} ({t})"
+        with open("notes.txt", "w") as fileHandler:
+            i = 0
+            while notes[i] != "":
+                fileHandler.writelines(f"{notes[i]}\n")
+                i += 1
+        notes = []
     elif a == "5":
-        notes[int(input("Enter index: ")) - 1].changeText(input("Enter text: "))
+        saveNotes(notes)
+        notes = getNotes()
+        i = int(input("Enter index: ")) - 1
+        note = notes[i].split(" ")
+        note[1] = input("Enter text: ")
+        t = str(datetime.now()).split(".")[0]
+        notes[i] = f"{note[0]} {note[1]} ({t})"
+        with open("notes.txt", "w") as fileHandler:
+            i = 0
+            while notes[i] != "":
+                fileHandler.writelines(f"{notes[i]}\n")
+                i += 1
+        notes = []
     elif a == "6":
+        notes = saveNotes(notes)
+    elif a == "7":
+        saveNotes(notes)
         print("Goodbye")
         break
